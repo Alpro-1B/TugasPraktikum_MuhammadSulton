d24011ed5e372a20402bb28cc5f9a0dd8675d96e
Sulton1303 <mhammadslton46@gmail.com>
2023-10-29 06:28:32 +0700

    Menambahkan folder Pertemuan-3

---
+++ Pertemuan-1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout <<"-------------------------------------------------------------------" <<endl;
+    cout <<"Nama : Muhammad Sulton" <<endl <<endl;
+    cout <<"NPM : 2310631170104" <<endl <<endl;
+    cout <<"Kelas : B - Informatika" <<endl <<endl;
+    cout <<"Cita-cita : Programmer" <<endl <<endl;
+    cout <<"Motivasi masuk program studi : Karena tertarik dengan dunia IT" <<endl <<endl;
+    cout <<"-------------------------------------------------------------------";
+
+    return 0;
+}

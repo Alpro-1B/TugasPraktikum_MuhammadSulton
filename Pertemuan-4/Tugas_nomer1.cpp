78a5475ce8cc645de3b44e41b83cc8c68cc6931f
Sulton1303 <mhammadslton46@gmail.com>
2023-11-20 01:16:55 +0700

    Commit pesan Anda di sini

--- Pertemuan-4/Tugas_nomer1.cpp
+++ Pertemuan-4/Tugas_nomer1.cpp
@@ -3,9 +3,10 @@
 using namespace std;
 
 int main() {
-    // Menginput bilangan mana yang ingin dicari oleh user, apakah ganjil, genap, atau prima.
+    //Variabel untuk input yang dilakukan oleh user
     int pilihanUser;
 
+    //Input yang dilakukan oelh user
     cout << "  Selamat datang di program mencari bilangan ganjil, genap, prima, serta faktornya" << endl;
     cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl << endl;
 
@@ -16,8 +17,9 @@ int main() {
     cout << "Pilihlah bilangan di atas yang ingin anda cari : ";
     cin >> pilihanUser;
 
-    // Operasi penyelesaian mencari bilangan ganjil, genap, dan prima
+    //Disini saya ingin menggunakan switch case agar output yang keluar menjadi lebih rapih
     switch (pilihanUser) {
+        //Operasi penyelesaian untuk bilangan ganjil
         case 1: {
             int batas;
             cout << "Anda memilih bilangan ganjil" << endl;
@@ -27,14 +29,26 @@ int main() {
                 cout << "batas harus lebih dari 0" << endl;
             } else {
                 cout << "bilangan ganjil dari 1 hingga " << batas << " adalah : ";
+                int jumlahGanjil = 0;
                 for (int i = 1; i <= batas; i += 2) {
                     cout << i << " ";
+                    jumlahGanjil += i;
+                }
+                cout << endl;
+                cout << "Jumlah bilangan ganjil: " << jumlahGanjil << endl;
+
+                cout << "Faktor-faktor bilangan ganjil dari 1 hingga " << batas << " adalah: ";
+                for (int i = 1; i <= batas; i++) {
+                    if (batas % i == 0) {
+                        cout << i << " ";
+                    }
                 }
                 cout << endl;
             }
             break;
         }
 
+        //Operasi penyelesaian untuk bilangan genap
         case 2: {
             int batas;
             cout << "Anda memilih bilangan genap" << endl;
@@ -44,14 +58,26 @@ int main() {
                 cout << "batas harus lebih dari 0" << endl;
             } else {
                 cout << "bilangan genap dari angka 2 hingga " << batas << " adalah : ";
+                int jumlahGenap = 0;
                 for (int j = 2; j <= batas; j += 2) {
                     cout << j << " ";
+                    jumlahGenap += j;
+                }
+                cout << endl;
+                cout << "Jumlah bilangan genap: " << jumlahGenap << endl;
+
+                cout << "Faktor-faktor bilangan genap dari 2 hingga " << batas << " adalah: ";
+                for (int j = 2; j <= batas; j += 2) {
+                    if (batas % j == 0) {
+                        cout << j << " ";
+                    }
                 }
                 cout << endl;
             }
             break;
         }
 
+        //Operasi penyelesaian untuk bilangan prima
         case 3: {
             int batas;
 
@@ -61,6 +87,7 @@ int main() {
             } while (batas <= 0);
 
             cout << "Bilangan prima antara 1 dan " << batas << " adalah: ";
+            int jumlahPrima = 0;
 
             for (int k = 2; k <= batas; ++k) {
                 int bilanganPrima = 1;
@@ -72,16 +99,26 @@ int main() {
                 }
                 if (bilanganPrima == 1) {
                     cout << k << " ";
+                    jumlahPrima += k;
                 }
             }
+            cout << endl;
+            cout << "Jumlah bilangan prima: " << jumlahPrima << endl;
 
+            cout << "Faktor-faktor bilangan prima dari 1 hingga " << batas << " adalah: ";
+            for (int k = 2; k <= batas; ++k) {
+                if (batas % k == 0) {
+                    cout << k << " ";
+                }
+            }
+            cout << endl;
             break;
         }
-
+        //Jika angka yang dimasukkan tidak sesuai maka output yang muncul adalah kalimat dibawah ini
         default:
-            cout << "Maaf, angka yang anda input tidak valid. Silahkan coba lagi.";
+            cout << "Angka yang anda masukkan tidak valid, yang benar dong input angkanya masbro.";
             break;
     }
 
-    return 0;   //Menandakan bahwa program telah berakhir.
+    return 0;   //Menandakan bahwa program telah berakhir
 }
